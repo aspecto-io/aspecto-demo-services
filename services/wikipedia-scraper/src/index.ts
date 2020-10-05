8076fb0c42d0f8db9ba4cd4ac8a258c0078e3125
Amir Blum <amir@aspecto.io>
2020-10-05 15:47:03 +0300

    feat: initiate scraper polling from http endpoint

--- services/wikipedia-scraper/src/index.ts
+++ services/wikipedia-scraper/src/index.ts
@@ -4,6 +4,7 @@ init({
   logger: console,
 });
 import axios from "axios";
+import express from "express";
 import { SQS } from "aws-sdk";
 
 let queueUrl;
@@ -20,53 +21,87 @@ const initSqs = async () => {
   console.log("sqs send queue ready", { queueUrl });
 }
 
-// set the search term which is going to be scraped from wikipedia
-// the default (if not set as env variable) is to search articles
-// with the value 'javascript' in the title.
-const searchTerm: string = process.env.WIKI_SEARCH_TERM || "title:javascript";
-
 // how many wiki pages to return in each batch (api call)
-const batchSize: number = +process.env.WIKI_SCRAPER_BATCH_SIZE || 5;
+const batchSize: number = +process.env.WIKI_SCRAPER_BATCH_SIZE || 4;
 
-// how many milliseconds to wait between each batch query on wikipedia api.
-const waitBetweenBatches: number =
-  +process.env.WIKI_SCRAPER_WAIT_BETWEEN_BATCHES_MS || 60000;
+const pollWikipediaArticles = async (offset: number, searchTerm: string): Promise<number> => {
+  const res = await axios.get("https://en.wikipedia.org/w/api.php", {
+    params: {
+      action: "query",
+      list: "search",
+      srsearch: searchTerm,
+      srlimit: batchSize,
+      srnamespace: 0, // search only articles
+      format: "json",
+      sroffset: offset,
+    },
+  });
 
-const pollWikipediaArticles = async (offset: number): Promise<number> => {
-  try {
-    const res = await axios.get("http://en.wikipedia.org/w/api.php", {
-      params: {
-        action: "query",
-        list: "search",
-        srsearch: searchTerm,
-        srlimit: batchSize,
-        srnamespace: 0, // search only articles
-        format: "json",
-        sroffset: offset,
-      },
-    });
+  await Promise.all(res.data.query.search.map(article =>
+    sqs.sendMessage({
+      QueueUrl: queueUrl,
+      MessageBody: article.title
+    }).promise()
+  ));
 
-    await Promise.all(res.data.query.search.map(article =>
-      sqs.sendMessage({
-        QueueUrl: queueUrl,
-        MessageBody: article.title
-      }).promise()
-    ));
-
-    const continueOffset = res.data.continue?.sroffset;
-    setTimeout( () => pollWikipediaArticles(continueOffset), waitBetweenBatches);
-    return continueOffset;
-
-  } catch (e) {
-    console.log("failed to fetch wikipedia articles", e);
-    setTimeout( () => pollWikipediaArticles(offset), waitBetweenBatches);
-  }
+  const continueOffset = res.data.continue?.sroffset;
+  return continueOffset;
 };
+
+const app = express();
+app.use(async (req, res, next) => {
+  try {
+    const { token } = req.query;
+    if (token) {
+      const userResponse = await axios({
+        url: `http://user:8080/user/token?token=${token}`,
+      });
+      if (userResponse.data) {
+        res.locals.user = userResponse.data;
+        next();
+      } else {
+        res.sendStatus(401);
+      }
+    } else {
+      res.sendStatus(401);
+    }
+  } catch (e) {
+    console.error(e.message, e);
+    res.sendStatus(500);
+  }
+});
+
+const continueOffsetBySearchTerm: Record<string, number> = {};
+
+const pollRouter = express.Router();
+
+pollRouter.get("/:searchTerm", async (req, res) => {
+  const searchTerm = req.params.searchTerm;
+  try {
+    if(!(searchTerm in continueOffsetBySearchTerm)) {
+      console.log(`request to poll search term ${searchTerm} for the first time. setting continue offset to 0`);
+      continueOffsetBySearchTerm[searchTerm] = 0;
+    }
+    const continueOffset = continueOffsetBySearchTerm[searchTerm];
+    const newContinueOffset = await pollWikipediaArticles(continueOffset, searchTerm);
+    continueOffsetBySearchTerm[searchTerm] = newContinueOffset;
+
+    res.status(200).send({
+      totalArticlesRead: newContinueOffset,
+      batchArticlesRead: newContinueOffset - continueOffset
+    });
+  } catch (e) {
+    console.log("failed to get poll article from wikipedia", e);
+    res.status(500);
+  }
+});
+
+app.use("/poll", pollRouter);
 
 (async () => {
   try {
     await initSqs();
-    pollWikipediaArticles(0);
+    app.listen(8080);
     console.log("wikipedia scraper started");
   } catch(e) {
     console.log('failed to init service', e);
