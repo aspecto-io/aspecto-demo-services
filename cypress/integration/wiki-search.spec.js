a86beb20cf541fc8ce8873ebd0fc0e52572fca58
Amir Blum <amir@aspecto.io>
2021-06-27 15:41:42 +0300

    ci: add cypress testing

---
+++ cypress/integration/wiki-search.spec.js
@@ -0,0 +1,10 @@
+
+describe('aspecto wiki demo', () => {
+    it('login', () => {
+        cy.visit('http://localhost:3002');
+
+        cy.get('#email').type('wikipedia-demo@aspecto.io');
+        cy.get('#password').type('Aspecto123');
+        cy.get('button').click(); 
+    })
+});
