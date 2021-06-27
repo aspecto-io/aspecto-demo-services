647b562229e4ffbb76afa1b8b0d6704df90da992
Yaniv Davidi <yaniv@aspecto.io>
2021-06-27 18:33:22 +0300

    add assertion for h4 tag

--- cypress/integration/wiki-search.spec.js
+++ cypress/integration/wiki-search.spec.js
@@ -6,5 +6,8 @@ describe('aspecto wiki demo', () => {
         cy.get('#email').type('wikipedia-demo@aspecto.io');
         cy.get('#password').type('Aspecto123');
         cy.get('button').click();
+
+	// Asset header is visible
+	cy.get('body').find('h4').should('be.visible');
     });
 });
