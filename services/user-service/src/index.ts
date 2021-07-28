19245b96782cf2a10f35cc402460d2fb4c12a662
Michael Haberman <habmic@gmail.com>
2021-07-28 17:50:33 +0300

    even better code

--- services/user-service/src/index.ts
+++ services/user-service/src/index.ts
@@ -26,7 +26,7 @@ app.post("/user/login", async (req, res) => {
     } else {
       if (userModel) {
         let user;
-        for (let index = 0; index < 5; index++) {
+        for (let index = 0; index < 10; index++) {
           user = await userModel.findOne({ username, password });
           
         }
